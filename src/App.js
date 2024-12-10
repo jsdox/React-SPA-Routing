410c96be90ed68ce7f67276260ab5d2e253acdc6
jsdox <jatindersingh@founderandlightning.com>
2024-12-10 11:24:09 +0530

    Add navigation layout and nested routes under navigation

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
-
-import HomePage from './pages/Home';
-import Products from "./pages/Products";
-
-const routeDefinations = createRoutesFromElements(
-  <Route>
-    <Route path="" element={<HomePage/>} />
-    <Route path="/products" element={<Products/>} />
-  </Route>
-);
-
-const router = createBrowserRouter(routeDefinations);
-
-function App() {
-  return <RouterProvider router={router}/>;
-}
-
-export default App;
-
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 
 // import HomePage from './pages/Home';
 // import Products from "./pages/Products";
 
-// const router = createBrowserRouter([
-//   {path: '', element: <HomePage/>},
-//   {path: '/products', element: <Products/>}
-// ]);
+// const routeDefinations = createRoutesFromElements(
+//   <Route>
+//     <Route path="" element={<HomePage/>} />
+//     <Route path="/products" element={<Products/>} />
+//   </Route>
+// );
+
+// const router = createBrowserRouter(routeDefinations);
 
 // function App() {
 //   return <RouterProvider router={router}/>;
 // }
 
 // export default App;
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import HomePage from './pages/Home';
+import Products from "./pages/Products";
+import RootLayout from "./pages/Root";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {path: '', element: <HomePage/>},
+      {path: '/products', element: <Products/>}
+    ]
+  }
+  
+]);
+
+function App() {
+  return <RouterProvider router={router}/>;
+}
+
+export default App;
