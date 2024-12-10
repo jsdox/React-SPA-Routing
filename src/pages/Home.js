e63bed569926b088ea595b23d6df0db1f46b94bd
jsdox <jatindersingh@founderandlightning.com>
2024-12-10 11:06:53 +0530

    Add link to route pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,13 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
     return (
-        <h1>My Home Page</h1>
+        <>        
+            <h1>My Home Page</h1>
+            <p>
+                Go to <Link to="/products">List of products</Link>
+            </p>
+        </>
     )
 
 }
