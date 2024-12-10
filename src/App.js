1881438fab316d6817031464cd65e1823df6c7ba
jsdox <jatindersingh@founderandlightning.com>
2024-12-10 11:03:12 +0530

    Alternative way to define the route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 
 import HomePage from './pages/Home';
 import Products from "./pages/Products";
 
-const router = createBrowserRouter([
-  {path: '', element: <HomePage/>},
-  {path: '/products', element: <Products/>}
-]);
+const routeDefinations = createRoutesFromElements(
+  <Route>
+    <Route path="" element={<HomePage/>} />
+    <Route path="/products" element={<Products/>} />
+  </Route>
+);
+
+const router = createBrowserRouter(routeDefinations);
 
 function App() {
   return <RouterProvider router={router}/>;
 }
 
 export default App;
+
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// import HomePage from './pages/Home';
+// import Products from "./pages/Products";
+
+// const router = createBrowserRouter([
+//   {path: '', element: <HomePage/>},
+//   {path: '/products', element: <Products/>}
+// ]);
+
+// function App() {
+//   return <RouterProvider router={router}/>;
+// }
+
+// export default App;
