ac0d33a4bde11c60bd19f35864a3fbb952376dbf
Jatinder Singh <jatindersingh@founderandlightning.com>
2025-07-15 13:02:41 +0530

    Products added

--- src/pages/ProductSingleList.js
+++ src/pages/ProductSingleList.js
@@ -5,6 +5,10 @@ const PRODUCTS = [
     { id: 'p2', title: 'Product 2' },
     { id: 'p3', title: 'Product 3' },
     { id: 'p4', title: 'Product 4' },
+    { id: 'p5', title: 'Product 5' },
+    { id: 'p6', title: 'Product 6' },
+    { id: 'p7', title: 'Product 7' }
+    { id: 'p8', title: 'Product 8' }
 { id: 'p5', title: 'Product 5' }
 ];
 
