b6cbcdde48e97d43b72c4725569d20c624506fb1
Jatinder Singh <jatindersingh@founderandlightning.com>
2025-07-15 13:02:02 +0530

    Fix error

--- src/pages/ProductSingleList.js
+++ src/pages/ProductSingleList.js
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 const PRODUCTS = [
     { id: 'p1', title: 'Product 1' },
     { id: 'p2', title: 'Product 2' },
-    { id: 'p3', title: 'Product 3' }
-    { id: 'p4', title: 'Product 4' }
+    { id: 'p3', title: 'Product 3' },
+    { id: 'p4', title: 'Product 4' },
 { id: 'p5', title: 'Product 5' }
 ];
 
